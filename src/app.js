c222beb75c8179cc5d7d7aa935182dad58b0c991
Jesse Tran <jessetran03@gmail.com>
2021-01-08 17:09:22 -0600

    updated app error

--- src/app.js
+++ src/app.js
@@ -34,10 +34,10 @@ app.get('/', (req, res) => {
 
 app.use(function errorHandler(error, req, res, next) {
   let response
+    console.error(error)
   if (NODE_ENV === 'production') {
     response = { error: { message: 'server error' } }
   } else {
-    console.error(error)
     response = { message: error.message, error }
   }
   res.status(500).json(response)
