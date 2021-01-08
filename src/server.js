f76909a33c0540c0f98a6585d65a1cb5251f9c89
Jesse Tran <jessetran03@gmail.com>
2021-01-08 17:23:03 -0600

    updated server

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const { PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL,
+  connection: DATABASE_URL + '?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory',
 })
 
 app.set('db', db)
